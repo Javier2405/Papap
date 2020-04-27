ecb996fd2e6433c097555a7819f6e601dad252a9
francothegenius <A01634495@itesm.mx>
2020-04-26 21:21:50 -0500

    firebase storing users

--- app/src/main/java/com/example/papap/Register.java
+++ app/src/main/java/com/example/papap/Register.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,17 +15,27 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.opencensus.tags.Tag;
 
 public class Register extends AppCompatActivity {
 
-    EditText fullNameRF, emailRF, passwordRF;
+    EditText fullNameRF, emailRF, passwordRF, phoneRF;
     Button registerButton;
     TextView login;
     ProgressBar progressBar;
     FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +46,13 @@ public class Register extends AppCompatActivity {
         fullNameRF = findViewById(R.id.fullNameFieldR);
         emailRF = findViewById(R.id.emailFieldR);
         passwordRF = findViewById(R.id.passRegister);
+        phoneRF = findViewById(R.id.phoneField);
         registerButton = findViewById(R.id.registerButton);
         login = findViewById(R.id.loginR);
         progressBar = findViewById(R.id.progressBarR);
         //initialize firebase
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
         if (fAuth.getCurrentUser() != null){
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
@@ -49,9 +62,15 @@ public class Register extends AppCompatActivity {
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = emailRF.getText().toString();
+                final String fullName = fullNameRF.getText().toString();
+                final String email = emailRF.getText().toString();
                 String password = passwordRF.getText().toString();
+                final String phone = phoneRF.getText().toString();
 
+                if(TextUtils.isEmpty(fullName)){
+                    fullNameRF.setError("Nombre completo es requerido");
+                    return;
+                }
                 if(TextUtils.isEmpty(email)){
                     emailRF.setError("Correo es requerido");
                     return;
@@ -61,8 +80,8 @@ public class Register extends AppCompatActivity {
                     return;
                 }
 
-                if (password.length() < 6){
-                    passwordRF.setError("Contraseña deber ser al menos de 6 caracteres");
+                if (password.length() <= 8){
+                    passwordRF.setError("Contraseña deber ser al menos de 8 caracteres");
                     return;
                 }
 
@@ -76,6 +95,25 @@ public class Register extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()){
                             Toast.makeText(Register.this, "Usuario creado exitosamente", Toast.LENGTH_SHORT).show();
+                            //create collection for storing
+                            //if collection doesnt exist it will create
+                            //we are going to insert the user info in a document by its id
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+                            //store the data
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("Nombre", fullName);
+                            user.put("Correo Electronico", email);
+                            user.put("Telefono", phone);
+                            //insert to data base
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d("INSERTION", "onSuccess: user profile is created for "+ userID);
+                                }
+                            });
+
+
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         }else {
                             Toast.makeText(Register.this, "Hubo un error al crear al usuario. Intente de nuevo", Toast.LENGTH_SHORT).show();
