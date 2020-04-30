c877f1931d3faa512bafe24436dcf34d9054cac0
Alan Gonzalez <argaalan@hotmail.com>
2020-04-30 11:13:12 -0600

    Added one new activity

---
+++ app/src/main/java/com/example/papap/Paps.java
@@ -0,0 +1,14 @@
+package com.example.papap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Paps extends AppCompatActivity {
+    ///View para las papillas
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paps);
+    }
+}
